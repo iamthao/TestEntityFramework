0b5d696dba3e00a9b019798a9760fcc1add06a36
nnthao <nnt.ntc@gmail.com>
2019-06-09 18:34:02 +0700

    first

---
+++ TestEntityFramework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new TestDbContext())
+            {
+                var grade = (from s in db.Grades
+                             select s).ToList();
+
+                Console.WriteLine("Xong");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TestEntityFramework/TestDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestEntityFramework
+{
+    public class TestDbContext : DbContext
+    {
+        public TestDbContext() : base("AdminDb") { }
+        public virtual DbSet<Grade> Grades { get; set; }
+    }
+}
